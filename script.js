fbb4c86de7c821f7f41ea00fa0251a91a8d24521
podnas <73196327+podnas@users.noreply.github.com>
2021-07-13 22:56:12 -0400

    updated to break out ingredients

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ DRINK_API_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
 
 const searchDrinksAPI = (drinkName) => {
     api_query = DRINK_API_URL + drinkName
+    console.log(api_query)
     fetch(api_query)
         .then(response => {
             console.log(response.status)
@@ -24,18 +25,54 @@ const searchDrinksAPI = (drinkName) => {
         })
 }
 
-// document.getElementById("drinkInfo")
+const removeEmpty = (obj) => {
+    Object.keys(obj).forEach((k) => (!obj[k] && obj[k] !== undefined) && delete obj[k]);
+    return obj;
+};
 
-// const singleDrink = (drinkName) => {
-//     let foo = searchDrinksAPI(drinkName);
-//     console.log(foo)
-//     // let output = document.getElementById("output").innerHTML = JSON.stringify(foo)
-// }
+const searchSingleDrink = (drinkName) => {
+    api_query = DRINK_API_URL + drinkName
+    console.log(api_query)
+    fetch(api_query)
+        .then(response => {
+                console.log(response.status)
+                if (response.status !== 200) {
+                    // console(response.status)
+                }
+                return response.json()
+            }
+        )
+        .then(data => {
+            let dataReturn = data.drinks[0];
+            for (let i = 1 ; i < 15; i++) {
+                ingredient = "strIngredient" + i;
+                if(data.drinks[0][ingredient]){
+                    console.log(data.drinks[0][ingredient])
+                }
+            }
 
-// searchDrinksAPI("Margarita")
 
 
-// let myVar = searchDrinksAPI("Margarita")
 
-console.log(searchDrinksAPI("Margarita"))
+                // console.log()
+
+
+            // console.log(i.length)
+            console.log(drinkIngre)
+            const drinkDetails = {
+                name : dataReturn.strDrink,
+                glassType : dataReturn.strGlass,
+                instructions: dataReturn.strInstructions,
+                thumbnail: dataReturn.strDrinkThumb
+            }
+            // console.log(drinkDetails)
+            // let output = document.getElementById("output").innerHTML = JSON.stringify(data.drinks[0].strCategory)
+            return data.drinks
+        })
+}
+
+
+
+// console.log(searchDrinksAPI("Margarita"))
+console.log(searchSingleDrink("Margarita"))
 
