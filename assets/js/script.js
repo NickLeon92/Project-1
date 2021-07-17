ba0d56fc5b1f4ff29c81461ae66dd83403605930
podnas <73196327+podnas@users.noreply.github.com>
2021-07-16 20:41:31 -0400

    added random drink function, minor cleanup of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-//let requestUrl = "https://en.wikipedia.org/w/api.php?action=parse&page=margarita&prop=wikitext&section=1&format=json&origin=*" 
-//let requestUrl = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=margarita&origin=*" 
 DRINK_API_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
 let description = document.getElementById("description")
 let output = document.getElementById("output")
@@ -10,6 +8,45 @@ let drinkDescripVal;
 let inputText;
 let requestUrl
 
+
+const searchRandomDrink = () => {
+    api_query = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+    fetch(api_query)
+        .then(response => {
+                if (response.status !== 200) {
+                    // console(response.status)
+                }
+                return response.json()
+            }
+        )
+        .then(data => {
+            let dataReturn = data.drinks;
+            let alcohol_data = []
+            for (let i = 1 ; i < 16; i++) {
+                ingredient = "strIngredient" + i;
+                if(data.drinks[0][ingredient]){
+                    alcohol_data.push(data.drinks[0][ingredient])
+                }
+            }
+            let alcohol_amount_data = []
+            for (let i = 1 ; i < 16; i++) {
+                ingredients = "strMeasure" + i;
+                if(data.drinks[0][ingredients]){
+                    alcohol_amount_data.push(data.drinks[0][ingredients])
+                }
+            }
+
+            let combinedIngredients = alcohol_data.map((x, i) => [x, alcohol_amount_data[i]])
+            const drinkDetails = {
+                name : dataReturn.strDrink,
+                glassType : dataReturn.strGlass,
+                instructions: dataReturn.strInstructions,
+                thumbnail: dataReturn.strDrinkThumb,
+                ingredients: combinedIngredients
+
+            }
+        })
+}
 
 const searchSingleDrink = (drinkName) => {
     console.log("drink api start")
@@ -52,11 +89,6 @@ const searchSingleDrink = (drinkName) => {
             }
             console.log(combinedIngredients)
             renderIngredients(drinkDetails)
-
-            
-
-
-           // return document.getElementById("output").innerHTML = JSON.stringify(drinkDetails)
         })
 }
 
@@ -66,8 +98,6 @@ btn.addEventListener("click",()=>{
     requestUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/"+ inputText
     getApi(requestUrl)},
     )
-
-
 
   function getApi(url) {
     fetch(url)
@@ -84,7 +114,6 @@ btn.addEventListener("click",()=>{
 
   }
   
-
 function render(){
     description.innerHTML = ""
 
@@ -127,7 +156,4 @@ function renderIngredients(drinkDetails){
 
     Pic = document.getElementById("pic")
     Pic.setAttribute("src",drinkDetails.thumbnail)
-    // output.appendChild(ingPic)
-    // ingPic.setAttribute("")
-
 }
