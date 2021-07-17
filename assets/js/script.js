302b1c08e55e10ed1026b25a83529a9d60c68400
podnas <73196327+podnas@users.noreply.github.com>
2021-07-17 11:16:11 -0400

    started genral code cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,6 @@ let inputText;
 let requestUrl
 let call = false
 
-
 const searchRandomDrink = () => {
     api_query = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
     fetch(api_query)
@@ -23,41 +22,41 @@ const searchRandomDrink = () => {
         )
         .then(data => {
             let dataReturn = data.drinks[0];
-            console.log(dataReturn)
+            // console.log(dataReturn)
             let alcohol_data = []
-            for (let i = 1 ; i < 16; i++) {
+            for (let i = 1; i < 16; i++) {
                 ingredient = "strIngredient" + i;
-                if(data.drinks[0][ingredient]){
+                if (data.drinks[0][ingredient]) {
                     alcohol_data.push(data.drinks[0][ingredient])
                 }
             }
             let alcohol_amount_data = []
-            for (let i = 1 ; i < 16; i++) {
+            for (let i = 1; i < 16; i++) {
                 ingredients = "strMeasure" + i;
-                if(data.drinks[0][ingredients]){
+                if (data.drinks[0][ingredients]) {
                     alcohol_amount_data.push(data.drinks[0][ingredients])
                 }
             }
 
             let combinedIngredients = alcohol_data.map((x, i) => [x, alcohol_amount_data[i]])
             const drinkDetails = {
-                name : dataReturn.strDrink,
-                glassType : dataReturn.strGlass,
+                name: dataReturn.strDrink,
+                glassType: dataReturn.strGlass,
                 instructions: dataReturn.strInstructions,
                 thumbnail: dataReturn.strDrinkThumb,
                 ingredients: combinedIngredients
 
             }
-            call =true
+            call = true
             renderIngredients(drinkDetails)
-            console.log(drinkDetails)
-            requestUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/"+ drinkDetails.name
+            // console.log(drinkDetails)
+            requestUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/" + drinkDetails.name
             getApi(requestUrl)
         })
 }
 
 const searchSingleDrink = (drinkName) => {
-    console.log("drink api start")
+    // console.log("drink api start")
     api_query = DRINK_API_URL + drinkName
     fetch(api_query)
         .then(response => {
@@ -70,111 +69,100 @@ const searchSingleDrink = (drinkName) => {
         .then(data => {
             let dataReturn = data.drinks[0];
             let alcohol_data = []
-            for (let i = 1 ; i < 16; i++) {
+            for (let i = 1; i < 16; i++) {
                 ingredient = "strIngredient" + i;
-                if(data.drinks[0][ingredient]){
+                if (data.drinks[0][ingredient]) {
                     alcohol_data.push(data.drinks[0][ingredient])
                 }
             }
             let alcohol_amount_data = []
-            for (let i = 1 ; i < 16; i++) {
+            for (let i = 1; i < 16; i++) {
                 ingredients = "strMeasure" + i;
-                if(data.drinks[0][ingredients]){
+                if (data.drinks[0][ingredients]) {
                     alcohol_amount_data.push(data.drinks[0][ingredients])
                 }
             }
 
             let combinedIngredients = alcohol_data.map((x, i) => [x, alcohol_amount_data[i]])
             const drinkDetails = {
-                name : dataReturn.strDrink,
-                glassType : dataReturn.strGlass,
+                name: dataReturn.strDrink,
+                glassType: dataReturn.strGlass,
                 instructions: dataReturn.strInstructions,
                 thumbnail: dataReturn.strDrinkThumb,
                 //directions: [combinedIngredients],
                 ingredients: combinedIngredients
                 // amount: [...alcohol_amount_data]
-
             }
-            console.log(combinedIngredients)
+            // console.log(combinedIngredients)
             renderIngredients(drinkDetails)
         })
 }
 
-btn.addEventListener("click",()=>{
-    call = false
-    inputText = search.value
-    console.log(inputText)
-    requestUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/"+ inputText
-    getApi(requestUrl)},
-    )
+btn.addEventListener("click", () => {
+        call = false
+        inputText = search.value
+        // console.log(inputText)
+        requestUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/" + inputText
+        getApi(requestUrl)
+    },
+)
 
-btn2.addEventListener("click",()=>{
-    searchRandomDrink()
-    //inputText = search.value
-    //console.log(inputText)
-    //requestUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/"+ inputText
-    //getApi(requestUrl)
-},
-        )
+btn2.addEventListener("click", () => {
+        searchRandomDrink()
+        //inputText = search.value
+        //console.log(inputText)
+        //requestUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/"+ inputText
+        //getApi(requestUrl)
+    },
+)
 
-
-  function getApi(url) {
+function getApi(url) {
     fetch(url)
-      .then(function (response) {
-    
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        drinkNameVal = data.title
-        drinkDescripVal = data.extract
-        render()
-      });
+        .then(function (response) {
 
-  }
-  
-function render(){
+            return response.json();
+        })
+        .then(function (data) {
+            // console.log(data);
+            drinkNameVal = data.title
+            drinkDescripVal = data.extract
+            render()
+        });
+
+}
+
+function render() {
     description.innerHTML = ""
-
     drinkName = document.createElement("h1")
     drinkDescrip = document.createElement("p")
-
     drinkName.textContent = drinkNameVal
     drinkDescrip.textContent = drinkDescripVal
-
     description.appendChild(drinkName)
     description.appendChild(drinkDescrip)
-
-    console.log(search.value)
-    if (!call){
-    searchSingleDrink(search.value)
+    // console.log(search.value)
+    if (!call) {
+        searchSingleDrink(search.value)
     }
 }
 
-function renderIngredients(drinkDetails){
+function renderIngredients(drinkDetails) {
     output.innerHTML = ""
-    console.log("render ingrdients start")
-
+    // console.log("render ingrdients start")
     ingHeader = document.createElement("h1")
     output.appendChild(ingHeader)
     ingHeader.textContent = "Ingredients:"
-
     ingList = document.createElement("ul")
     output.appendChild(ingList)
-
-
     output.appendChild(ingList)
 
-    for (i=0; i<drinkDetails.ingredients.length; i++){
+    for (i = 0; i < drinkDetails.ingredients.length; i++) {
         let listEl = document.createElement("li")
         output.appendChild(listEl)
         listEl.textContent = drinkDetails.ingredients[i]
     }
-
     ingDirections = document.createElement("p")
     output.appendChild(ingDirections)
     ingDirections.textContent = drinkDetails.instructions
-
     Pic = document.getElementById("pic")
-    Pic.setAttribute("src",drinkDetails.thumbnail)
+    Pic.setAttribute("src", drinkDetails.thumbnail)
 }
